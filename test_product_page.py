043589f09010411efa8a98b3ffa388ada4eaa13f
Анна Яковлева <nikvnik989@gmail.com>
2020-09-11 15:05:04 +0300

    Test mark parametrize and xfail

--- test_product_page.py
+++ test_product_page.py
@@ -10,8 +10,7 @@ import pytest
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail)])
 
 def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
